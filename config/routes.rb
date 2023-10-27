a03cfac03a338e956a327db0e85749d09b3db9b7
sahilshaikh7 <sahilshaikh3010@gmail.com>
2023-10-27 12:31:28 +0530

    eventclub-last-commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  root "home#index"
+  resources :events
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "home#index"
   get "/home", to: "home#index"
+  get "/about", to: "home#about"
+  get "/contact", to: "home#contact"
 end
